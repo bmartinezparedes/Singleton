9a641da33a0a74de9c1d21c847127032171158fa
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2020-12-01 10:36:32 +0100

    Añadimos nuestra instancia ('static') y nuestro método getInstance()...

--- src/com/programacion/Singleton.java
+++ src/com/programacion/Singleton.java
@@ -5,12 +5,26 @@ public class Singleton {
     private String nombre;
     private Integer edad;
 
+    //esta va a ser nuestra unica instancia
+    private static Singleton instance=null;
+
     /**
      * Constructor bloqueado con private
      */
 
     private Singleton() {}
 
+    public static Singleton getInstance(){
+        //comprueba si ya esta creada
+        if (instance==null) {
+            //como no está creada, la creo
+            //desde aqui si que puedo acceder al contructor
+            //por que estoy en la misma clase
+            instance = new Singleton();
+        }
+        // devuelvo la instancia
+        return instance;
+    }
         public void setEdad(Integer edad){
             this.edad=edad;
         }
